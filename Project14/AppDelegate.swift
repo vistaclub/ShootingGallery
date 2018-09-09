4b4989c1abec66a4dfcdafdbd469ae0c194c54dc
Jason Wong <jawong71@gmail.com>
2018-09-09 14:30:41 -0700

    Initial Commit

---
+++ Project14/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Project14
+//
+//  Created by Jason Wong on 2018-09-09.
+//  Copyright © 2018 Jason Wong. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
