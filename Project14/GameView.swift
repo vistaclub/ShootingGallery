2e68139ae630a3298d78ad694396e91a2d804bb8
Jason Wong <jawong71@gmail.com>
2018-09-29 18:57:57 -0700

    first commit

---
+++ Project14/GameView.swift
@@ -0,0 +1,19 @@
+//
+//  GameView.swift
+//  Project14
+//
+//  Created by Jason Wong on 2018-09-09.
+//  Copyright © 2018 Jason Wong. All rights reserved.
+//
+
+import Cocoa
+
+class GameView: SKView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
---
+++ Project14/Target.swift
@@ -0,0 +1,13 @@
+//
+//  Target.swift
+//  Project14
+//
+//  Created by Jason Wong on 2018-09-09.
+//  Copyright © 2018 Jason Wong. All rights reserved.
+//
+
+import Cocoa
+
+class Target: SKNode {
+
+}
